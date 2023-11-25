e0b27cb31fcb7b6df800d4976038a2a231331d58
miaslls <97785569+miaslls@users.noreply.github.com>
2023-11-25 17:21:21 -0300

    fix: lose theme when changing locale

--- app/_components/header/header.tsx
+++ app/_components/header/header.tsx
@@ -2,49 +2,15 @@
 
 import styles from './header.module.css';
 
-import { useChangeLocale, useCurrentLocale } from '@/locales/client';
 import SetTheme from './set-theme';
+import LangNav from './lang-nav/lang-nav';
 import MainNav from './main-nav/main-nav';
 
 export default function Header() {
-  const currentLocale = useCurrentLocale();
-  const changeLocale = useChangeLocale();
-
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <button
-              className={
-                styles.locale_button +
-                ' ' +
-                (currentLocale === 'en' ? styles.active_locale : null)
-              }
-              type="button"
-              onClick={() => changeLocale('en')}
-            >
-              en
-            </button>
-          </li>
-
-          <li>
-            <button
-              className={
-                styles.locale_button +
-                ' ' +
-                (currentLocale === 'pt' ? styles.active_locale : null)
-              }
-              type="button"
-              onClick={() => changeLocale('pt')}
-            >
-              pt
-            </button>
-          </li>
-        </ul>
-        <SetTheme />
-      </nav>
-
+      <LangNav />
+      <SetTheme />
       <MainNav />
     </header>
   );
