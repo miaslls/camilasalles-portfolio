47cbd6e347bd48bfa033f261af08038f00ae76aa
miaslls <97785569+miaslls@users.noreply.github.com>
2023-12-10 23:40:11 -0300

    home section, featured projects

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,8 @@ import styles from './page.module.css';
 import { setStaticParamsLocale } from 'next-international/server';
 import { getScopedI18n } from '@/locales/server';
 
+import FeaturedProjects_Home from './featured-projects_home';
+
 export default async function HomePage({
   params: { locale },
 }: {
@@ -13,33 +15,7 @@ export default async function HomePage({
 
   return (
     <main className={styles.main_home}>
-      <h2>{t('featured_projects')}</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ligula
-        tellus, gravida ut consequat quis, luctus nec neque. Fusce imperdiet
-        consectetur neque, sit amet imperdiet augue venenatis id. Proin et eros
-        leo. Morbi fermentum luctus ex, sit amet euismod est porta at. Duis
-        dictum hendrerit sagittis. Nam eget luctus turpis. Etiam consequat ante
-        ut sem volutpat blandit. Praesent in lacus convallis, tempor tellus in,
-        interdum lacus. Vivamus feugiat justo in vehicula pharetra. Pellentesque
-        lacus metus, bibendum eu viverra at, varius vitae massa. Fusce id
-        dapibus augue. Maecenas id risus laoreet, vestibulum ante ut, ultricies
-        dui.
-      </p>
-
-      <br />
-
-      <p>
-        Nam facilisis elementum enim nec fermentum. Etiam mollis erat sed nulla
-        fringilla, vel vestibulum elit congue. Mauris et dapibus nibh. Maecenas
-        bibendum auctor purus, vel rutrum lacus semper vitae. Aliquam gravida
-        nisl lorem, eget cursus nisi fermentum nec. Etiam ac interdum risus.
-        Vivamus hendrerit tortor vitae semper aliquam. Suspendisse porta nibh
-        sed enim congue, vitae bibendum est mollis. Vestibulum erat neque,
-        rhoncus et tellus vitae, vehicula vestibulum ante. Fusce massa odio,
-        condimentum non viverra ut, congue eget erat. Nam porttitor fermentum
-        mi, non placerat ligula. Fusce pretium ultrices fermentum.
-      </p>
+      <FeaturedProjects_Home />
     </main>
   );
 }
