3bb4268b3d4b69addf4e3a146ff9a2f621fe97cf
miaslls <me@camilasalles.dev>
2024-02-04 22:29:07 -0300

    fix: remove unused

--- app/_components/menu/ToggleThemeButton.tsx
+++ app/_components/menu/ToggleThemeButton.tsx
@@ -2,17 +2,10 @@
 
 import styles from './ToggleThemeButton.module.css';
 
-import Image from 'next/image';
 import { useScopedI18n } from '@/locales/client';
 import { useTheme } from '@providers/ThemeContextProvider';
-
 import SunIcon from '../icons/SunIcon';
 import MoonIcon from '../icons/MoonIcon';
-
-import sunIcon_light from '@icons/light/sun-fill.svg';
-import moonIcon_light from '@icons/light/moon-fill.svg';
-import sunIcon_dark from '@icons/dark/sun-fill.svg';
-import moonIcon_dark from '@icons/dark/moon-fill.svg';
 
 type ToggleThemeButtonProps = {
   isIconVariable: boolean;
@@ -25,14 +18,6 @@ export default function ToggleThemeButton({
 
   const { theme, toggleTheme } = useTheme();
   const isDarkTheme = theme === 'dark';
-
-  let moonIcon = moonIcon_light;
-  let sunIcon = sunIcon_light;
-
-  if (isIconVariable) {
-    moonIcon = isDarkTheme ? moonIcon_light : moonIcon_dark;
-    sunIcon = isDarkTheme ? sunIcon_light : sunIcon_dark;
-  }
 
   return (
     <button
